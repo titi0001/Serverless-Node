31951d0b6aef5e65ad00d4d7606c588b47785781
Thiago Pereira <titiura@gmail.com>
2023-10-20 12:00:05 -0300

    feat: add endpoint to interface- index.js- results.js

--- interface/js/index.js
+++ interface/js/index.js
@@ -1,42 +1,50 @@
-const $ = document.querySelector.bind(document)
+const $ = document.querySelector.bind(document);
 
-$('form').addEventListener('submit', async (e) => {
-  e.preventDefault()
+if (!window.localStorage.getItem("token")) {
+  window.location.href = "/login.html";
+}
+
+$("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
   const data = {
-    answers: []
+    answers: [],
+  };
+
+  data.name = $('input[name="student-name"').value;
+  if (data.name === "") {
+    alert("Por favor, preencha o nome do aluno");
+    return;
   }
 
-  data.name = $('input[name="student-name"').value
-  if (data.name === '') {
-    alert('Por favor, preencha o nome do aluno')
-    return
-  }
-
-  const selectedAnswers = document.querySelectorAll('input[type="radio"]:checked')
+  const selectedAnswers = document.querySelectorAll(
+    'input[type="radio"]:checked'
+  );
 
   if (selectedAnswers.length !== 4) {
-    alert('Você não respondeu todas as perguntas')
-    return
+    alert("Você não respondeu todas as perguntas");
+    return;
   }
 
   selectedAnswers.forEach((answer) => {
-    data.answers[Number(answer.name) - 1] = Number(answer.value)
-  })
+    data.answers[Number(answer.name) - 1] = Number(answer.value);
+  });
 
-  const response = await fetch('/api/results', {
-    method: 'POST',
+  const response = await fetch("https://uq4nxvom5h.execute-api.eu-west-1.amazonaws.com/api/results", {
+    method: "POST",
     body: JSON.stringify(data),
     headers: {
-      'Content-Type': 'application/json'
-    }
-  })
+      "Content-Type": "application/json",
+    },
+  });
 
   if (response.ok) {
-    const { __hypermedia } = await response.json()
+    const { __hypermedia } = await response.json();
 
     window.location = new URL(
-      __hypermedia.href + '?' + new URLSearchParams(__hypermedia.query).toString(),
+      __hypermedia.href +
+        "?" +
+        new URLSearchParams(__hypermedia.query).toString(),
       window.location
-    ).toString()
+    ).toString();
   }
-})
+});
--- interface/js/results.js
+++ interface/js/results.js
@@ -1,23 +1,27 @@
-const queryString = new URLSearchParams(window.location.search)
-const resultId = queryString.get('id')
-
-const bail = () => {
-  alert('Nenhum resultado encontrado')
-  // window.location = '/'
+if (!window.localStorage.getItem("token")) {
+  window.location.href = "/login.html";
 }
 
-if (!resultId) bail()
+const queryString = new URLSearchParams(window.location.search);
+const resultId = queryString.get("id");
 
-fetch(`/api/results/${resultId}`)
+const bail = () => {
+  alert("Nenhum resultado encontrado");
+  // window.location = '/'
+};
+
+if (!resultId) bail();
+
+fetch(`https://uq4nxvom5h.execute-api.eu-west-1.amazonaws.com/api/results${resultId}`)
   .then((r) => {
-    if (!r.ok) bail()
-    return r.json()
+    if (!r.ok) bail();
+    return r.json();
   })
   .then(({ result }) => {
-    document.getElementById('student-name').innerText = result.name
-    document.getElementById('correct').innerText = result.correctAnswers
+    document.getElementById("student-name").innerText = result.name;
+    document.getElementById("correct").innerText = result.totalCorrectAnswers;
   })
   .catch((e) => {
-    console.error(e)
-    bail()
-  })
+    console.error(e);
+    bail();
+  });
