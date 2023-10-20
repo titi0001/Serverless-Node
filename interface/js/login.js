10c1eab8813a1b247e95463075e6fb0c82c5933c
Thiago Pereira <titiura@gmail.com>
2023-10-20 17:05:39 -0300

    fix: fix name const username not userName

--- interface/js/login.js
+++ interface/js/login.js
@@ -1,19 +1,18 @@
 document.querySelector("#submit").addEventListener("click", async (e) => {
   e.preventDefault();
 
-  const userName = document.querySelector('input[name="username"]').value;
+  const username = document.querySelector('input[name="username"]').value;
   const password = document.querySelector('input[name="password"]').value;
 
-  if (!userName || !password) {
+  if (!username || !password) {
     alert("Please enter a username and password");
-    return;
   }
 
   const response = await fetch(
     "https://uq4nxvom5h.execute-api.eu-west-1.amazonaws.com/api/login",
     {
       method: "POST",
-      body: JSON.stringify({ userName, password }),
+      body: JSON.stringify({ username, password }),
       headers: { "Content-Type": "application/json" },
     }
   );
