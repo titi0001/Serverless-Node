943db9a0dfc3d57615eadccb32f06c41512e68a9
Thiago Pereira <titiura@gmail.com>
2023-10-24 11:07:02 -0300

    feat: add email validation

--- Serverless-II/api/src/cadastro_batch/convertDadosCsv.js
+++ Serverless-II/api/src/cadastro_batch/convertDadosCsv.js
@@ -1,23 +1,33 @@
 const { parse } = require("fast-csv");
 
 async function convertDadosCsv(dados) {
-  const resultado = await new Promise((resolve, reject) => {
+  const result = await new Promise((resolve, reject) => {
     const alunos = [];
 
+    const regexEmail = /^\w+([\.\+-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
     const stream = parse({ headers: ["nome", "email"], renameHeaders: true })
+      .validate((aluno) => regexEmail.test(aluno.email))
       .on("data", (aluno) => alunos.push(aluno))
-      .on("error", (error) =>
-        reject(new Error("Houve um erro no processamento do CSV."))
+      .on("data-invalid", (aluno) =>
+        rejeitar(
+          new Error(
+            `O email informado para o aluno ${aluno.nome} é inválido (${aluno.email})`
+          )
+        )
       )
-      .on("end", () => resolve(alunos));
+      .on("error", (erro) =>
+        rejeitar(new Error("Houve um erro no processamento do arquivo CSV."))
+      )
+      .on("end", () => resolver(alunos));
 
     stream.write(dados);
     stream.end();
   });
 
-  if (resultado instanceof Error) throw resultado;
+  if (result instanceof Error) throw result;
 
-  return resultado;
+  return result;
 }
 
 module.exports = {
