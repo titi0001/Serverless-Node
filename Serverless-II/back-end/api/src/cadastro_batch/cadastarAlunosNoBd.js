ffc666955724b9cee1ffcf75a9236107b9b0cdc8
Thiago Pereira <titiura@gmail.com>
2023-10-28 18:08:39 -0300

    feat: add files to back-end folder

---
+++ Serverless-II/back-end/api/src/cadastro_batch/cadastarAlunosNoBd.js
@@ -0,0 +1,18 @@
+async function cadastrarAlunosNoBd(alunos) {
+  const alunosPromessas = alunos.map((aluno) => {
+    return fetch("http://curso-serverless2-api-228541440.us-east-1.elb.amazonaws.com/alunos", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(aluno),
+    });
+  });
+
+  const result = await Promise.all(alunosPromessas);
+
+  if (result.some((res) => !res.ok))
+    throw new Error("Houve um erro no cadastro dos alunos.");
+}
+
+module.exports = {
+  cadastrarAlunosNoBd,
+};
