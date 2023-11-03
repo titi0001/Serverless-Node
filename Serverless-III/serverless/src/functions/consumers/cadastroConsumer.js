bb83e751a6cea033c0e50adb3a79f568d0d5c154
Thiago Pereira <titiura@gmail.com>
2023-11-03 16:29:36 -0300

    fix: function cadastroConsumer

--- Serverless-III/serverless/src/functions/consumers/cadastroConsumer.js
+++ Serverless-III/serverless/src/functions/consumers/cadastroConsumer.js
@@ -1,13 +1,13 @@
-const { cadatrarAlunos } = require('../cadastroAlunos/cadastrarAlunos');
+const { cadastrarAlunos } = require('../cadastroAlunos/cadastrarAlunos');
 
-module.exports.cadastroConsumer = async (events) => {
-  console.log('DADOS ENVENTO DO CONSUMER', events);
+module.exports.cadastroConsumer = async (evento) => {
+  console.log('DADOS EVENTO DO CONSUMER', evento);
 
   try {
-    const { body } = events.Records[0];
-    return await cadatrarAlunos(body);
-  } catch (error) {
-    console.log('falha no envio para cadastro');
-    console.error(error);
+    const { body } = evento.Records[0];
+    return cadastrarAlunos(body);
+  } catch (erro) {
+    console.error('falha no envio para cadastro');
+    throw erro;
   }
 };
