db97ccbe3a1517e1797dec90a43a1cae0fcd4e26
Thiago Pereira <titiura@gmail.com>
2023-11-03 19:46:50 -0300

    fix: remove console.log

--- Serverless-III/serverless/src/functions/consumers/cadastroConsumer.js
+++ Serverless-III/serverless/src/functions/consumers/cadastroConsumer.js
@@ -1,8 +1,6 @@
 const { cadastrarAlunos } = require('../cadastroAlunos/cadastrarAlunos');
 
 module.exports.cadastroConsumer = async (evento) => {
-  console.log('DADOS EVENTO DO CONSUMER', evento);
-
   try {
     const { body } = evento.Records[0];
     return cadastrarAlunos(body);
