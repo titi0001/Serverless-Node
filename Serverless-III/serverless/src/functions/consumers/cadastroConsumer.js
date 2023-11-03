72b34426d943cc1d219fc9d4fdeffbb97580fc74
Thiago Pereira <titiura@gmail.com>
2023-11-03 15:38:02 -0300

    feat: create function cadastroConsumer

--- Serverless-III/serverless/src/functions/consumers/cadastroConsumer.js
+++ Serverless-III/serverless/src/functions/consumers/cadastroConsumer.js
@@ -1,3 +1,13 @@
+const { cadatrarAlunos } = require('../cadastroAlunos/cadastrarAlunos');
+
 module.exports.cadastroConsumer = async (events) => {
-  console.log('DADOS ENVENTO DO CONSUMER',events);
+  console.log('DADOS ENVENTO DO CONSUMER', events);
+
+  try {
+    const { body } = events.Records[0];
+    return await cadatrarAlunos(body);
+  } catch (error) {
+    console.log('falha no envio para cadastro');
+    console.error(error);
+  }
 };
