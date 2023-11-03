b51f740e41ec6c0b1224e6040446bc54943b4147
Thiago Pereira <titiura@gmail.com>
2023-11-03 16:40:23 -0300

    fix: function cadastroProducer in serverless

--- Serverless-III/serverless/src/functions/producers/cadastroProducer.js
+++ Serverless-III/serverless/src/functions/producers/cadastroProducer.js
@@ -2,10 +2,11 @@ const { client } = require('../../../config/clientSQS');
 const { SendMessageCommand } = require('@aws-sdk/client-sqs');
 const crypto = require('crypto');
 
-module.exports.cadastroProducer = async (objAlunos) => {
+module.exports.cadastroProducer = async (objAluno) => {
+
   const input = {
     QueueUrl: process.env.SQS_QUEUE_CADASTRO_URL,
-    MessageBody: JSON.stringify(objAlunos),
+    MessageBody: JSON.stringify(objAluno),
     DelaySeconds: 0,
     MessageDeduplicationId: crypto.randomUUID(),
     MessageGroupId: 'cadastro',
@@ -13,9 +14,9 @@ module.exports.cadastroProducer = async (objAlunos) => {
 
   try {
     const command = new SendMessageCommand(input);
-    const response = await client.send(command);
-    console.log('Mensagem enviada com sucesso!', response.MessageId);
-    return response;
+    const mensagem = await client.send(command);
+    console.log('mensagem enviada com sucesso', mensagem.MessageId);
+    return mensagem;
 
   } catch (erro) {
     console.log(erro);
